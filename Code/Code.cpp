606ad07cee24f92d34de36b6bc6990ea9f028c4a
Никаев Малик Магамедович <1nmm@spbt.local>
2023-05-20 12:53:45 +0300

    First_commit(uml diagrams, code, code_catch2, test-plan~)

---
+++ Code/Code.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <locale>
+#include <array>
+
+using namespace std;
+
+class dist {
+private: 
+    float speed;
+    int time;
+public:
+    int setSpeed(float currentSpeed) {
+        int errorCode{ 0 };
+
+        if (currentSpeed < 1 || currentSpeed > 300) {
+            errorCode = -1;
+        } else {
+            speed = currentSpeed;
+        }
+        
+        return errorCode;
+    }
+    float getSpeed() {
+        return speed;
+    }
+
+    int setTime(int currentTime) {
+        int errorCode{ 0 };
+
+        if (currentTime < 1 || currentTime > 360) {
+            errorCode = -1;
+        } else {
+            time = currentTime;
+        }
+
+        return errorCode;
+    }
+    float getTime() {
+        return time;
+    }
+
+    int calculateDist() {
+        
+        int distance = (speed * 60) * time;
+
+        return distance;
+    }
+
+    string getAllValues() {
+
+        string result = to_string(speed) + " " + to_string(time);
+
+        return result;
+    }
+};
+
+int main() {
+    dist object;
+
+    cerr << object.setSpeed(231.25) << endl;
+    cerr << object.getSpeed() << endl << endl;
+
+    cerr << object.setTime(32) << endl;
+    cerr << object.getTime() << endl << endl; 
+
+    cerr << object.calculateDist() << endl << endl;
+
+    cerr << object.getAllValues();
+}
+
+
---
+++ Code_catch2/Code_catch2.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <string>
+#include <locale>
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+using namespace std;
+
+class dist {
+private:
+    float speed;
+    int time;
+public:
+    int setSpeed(float currentSpeed) {
+        int errorCode{ 0 };
+
+        if (currentSpeed < 1 || currentSpeed > 300) {
+            errorCode = -1;
+        }
+        else {
+            speed = currentSpeed;
+        }
+
+        return errorCode;
+    }
+    float getSpeed() {
+        return speed;
+    }
+
+    int setTime(int currentTime) {
+        int errorCode{ 0 };
+
+        if (currentTime < 1 || currentTime > 360) {
+            errorCode = -1;
+        }
+        else {
+            time = currentTime;
+        }
+
+        return errorCode;
+    }
+    float getTime() {
+        return time;
+    }
+
+    int calculateDist() {
+
+        int distance = (speed * 60) * time;
+
+        return distance;
+    }
+
+    string getAllValues() {
+
+        string result = to_string(speed) + " " + to_string(time);
+
+        return result;
+    }
+};
+
+
+TEST_CASE("setSpeed method testing") {
+    dist object;
+    SECTION("Positive tests") {
+        REQUIRE(object.setSpeed(1) == 0);
+        REQUIRE(object.setSpeed(300) == 0);
+        REQUIRE(object.setSpeed(100) == 0);
+    }
+    SECTION("Negative tests") {
+        REQUIRE(object.setSpeed(0) == -1);
+        REQUIRE(object.setSpeed(-1) == -1);
+        REQUIRE(object.setSpeed(301) == -1);
+    }
+}
+
+TEST_CASE("setTime method testing") {
+    dist object;
+    SECTION("Positive tests") {
+        REQUIRE(object.setTime(1) == 0);
+        REQUIRE(object.setTime(360) == 0);
+        REQUIRE(object.setTime(200) == 0);
+    }
+    SECTION("Negative tests") {
+        REQUIRE(object.setTime(0) == -1);
+        REQUIRE(object.setTime(-1) == -1);
+        REQUIRE(object.setTime(361) == -1);
+    }
+}
